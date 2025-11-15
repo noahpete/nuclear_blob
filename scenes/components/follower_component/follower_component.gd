125452a6f9d003fc4eca5ca704bc87168898d12b
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-11-14 17:40:33 -0800

    Add player visuals

---
+++ scenes/components/follower_component/follower_component.gd
@@ -0,0 +1,6 @@
+class_name FollowerComponent
+extends Node
+
+@export var leader: Node2D
+@export var follow_distance: float
+@export var damping: float
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var sprite_2d: Sprite2D = $Visuals/Sprite2D
+@onready var visuals: CanvasGroup = $Visuals
+@onready var globs: GPUParticles2D = $Visuals/Globs
+@onready var eyes_sprite_2d: Sprite2D = $Visuals/EyesSprite2D
 
 func _physics_process(delta: float) -> void:
 	var input_vector := Input.get_vector("left", "right", "up", "down")
@@ -11,10 +13,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		animation_player.play("move")
 	_update_skew(delta)
+	_update_eyes(delta)
+	_update_globs(delta)
 
 	velocity = input_vector * 100
 	move_and_slide()
 
 func _update_skew(delta: float) -> void:
 	var target_skew := deg_to_rad(Input.get_axis("left", "right") * 5)
-	sprite_2d.skew = lerp(sprite_2d.skew, target_skew, delta * 10.0)
+	visuals.skew = lerp(visuals.skew, target_skew, delta * 10)
+
+func _update_globs(delta: float) -> void:
+	var target_position := -1 * Input.get_vector("left", "right", "up", "down") * 2
+	globs.position = lerp(globs.position, target_position, delta * 10)
+
+func _update_eyes(delta: float) -> void:
+	var target_position := Input.get_vector("left", "right", "up", "down")
+	eyes_sprite_2d.position = lerp(eyes_sprite_2d.position, target_position, delta * 10)
