bf94909547e4934458fe863b79fcc88b9b6cf2fc
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-11-16 09:38:18 -0800

    Add factory tilemap, glob spawning system, lighting effects, and reorganize assets

--- scenes/components/follower_component/follower_component.gd
+++
@@ -1,6 +0,0 @@
-class_name FollowerComponent
-extends Node
-
-@export var leader: Node2D
-@export var follow_distance: float
-@export var damping: float
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+const GLOB = preload("uid://buyg2u6ftbuc4")
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var visuals: CanvasGroup = $Visuals
-@onready var globs: GPUParticles2D = $Visuals/Globs
+@onready var body_globs: GPUParticles2D = $Visuals/BodyGlobs
 @onready var eyes_sprite_2d: Sprite2D = $Visuals/EyesSprite2D
 
 func _physics_process(delta: float) -> void:
@@ -12,6 +14,7 @@ func _physics_process(delta: float) -> void:
 		animation_player.play("RESET")
 	else:
 		animation_player.play("move")
+
 	_update_skew(delta)
 	_update_eyes(delta)
 	_update_globs(delta)
@@ -25,8 +28,8 @@ func _update_skew(delta: float) -> void:
 
 func _update_globs(delta: float) -> void:
 	var target_position := -1 * Input.get_vector("left", "right", "up", "down") * 2
-	globs.position = lerp(globs.position, target_position, delta * 10)
+	body_globs.position = lerp(body_globs.position, target_position, delta * 10)
 
 func _update_eyes(delta: float) -> void:
-	var target_position := Input.get_vector("left", "right", "up", "down")
+	var target_position := Input.get_vector("left", "right", "up", "down") * 2
 	eyes_sprite_2d.position = lerp(eyes_sprite_2d.position, target_position, delta * 10)
