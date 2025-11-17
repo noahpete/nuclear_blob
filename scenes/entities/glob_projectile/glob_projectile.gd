682dbcdc5b1e46680188e5e741883bb59cd8da2d
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-11-17 08:21:11 -0800

    Add Rat death globs and glob projectiles

--- scenes/entities/glob_projectile/glob_projectile.gd
+++ scenes/entities/glob_projectile/glob_projectile.gd
@@ -1,7 +1,7 @@
-class_name Glob
+class_name GlobProjectile
 extends Sprite2D
 
-const SPEED: float = 300.0
+const SPEED: float = 50.0
 
 var direction: Vector2 = Vector2.ZERO
 
@@ -9,6 +9,7 @@ var direction: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	life_timer.timeout.connect(_on_life_timer_timeout)
+	_start_rotation()
 
 func _physics_process(delta: float) -> void:
 	global_position += direction * SPEED * delta
@@ -16,6 +17,11 @@ func _physics_process(delta: float) -> void:
 func initialize(input_direction: Vector2) -> void:
 	direction = input_direction
 	rotation = direction.angle()
+
+func _start_rotation() -> void:
+	var rotation_tween := create_tween()
+	var rotation_amount := randi_range(-360, 360)
+	rotation_tween.tween_property(self, "rotation_degrees", rotation_amount, life_timer.wait_time)
 
 func _on_life_timer_timeout() -> void:
 	queue_free()
