6edfbd9d990567c510a193c02a1935852c4a26d7
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-11-18 21:34:29 -0800

    Remove glob_ground_particles

--- scenes/effect/glob_ground_particles/glob_ground_particles.gd
+++
@@ -1,9 +0,0 @@
-class_name GlobGroundParticles
-extends Node2D
-
-@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
-
-func _ready() -> void:
-	gpu_particles_2d.emitting = true
-	await get_tree().create_timer(12).timeout
-	queue_free()
