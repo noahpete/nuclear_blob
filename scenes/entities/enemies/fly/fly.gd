2f0f6d5a5f26eec56806776d84832451493aa68b
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-11-19 18:02:52 -0800

    Use new VelocityComponent

--- scenes/entities/enemies/fly/fly.gd
+++ scenes/entities/enemies/fly/fly.gd
@@ -1,5 +1,13 @@
 class_name Fly
 extends CharacterBody2D
 
-func _process(delta: float) -> void:
-	pass
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+@onready var visuals: Node2D = $Visuals
+
+func _process(_delta: float) -> void:
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
--- scenes/entities/enemies/rat/rat.gd
+++ scenes/entities/enemies/rat/rat.gd
@@ -1,11 +1,21 @@
 class_name Rat
 extends CharacterBody2D
 
+@onready var velocity_component: VelocityComponent = $VelocityComponent
 @onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
 @onready var visuals: Node2D = $Visuals
 
 func _ready() -> void:
 	hurtbox_component.hit.connect(_on_hit)
+
+
+func _process(_delta: float) -> void:
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(move_sign, 1)
 
 func _on_hit() -> void:
 	for child in visuals.get_children():
