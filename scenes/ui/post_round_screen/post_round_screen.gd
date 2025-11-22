5d5814c027d9e84f7e9aaf957c620c198c531902
Noah Peters <97405316+noahpete@users.noreply.github.com>
2025-11-22 10:46:03 -0800

    Add game restart

--- scenes/ui/post_round_screen/post_round_screen.gd
+++ scenes/ui/post_round_screen/post_round_screen.gd
@@ -3,13 +3,20 @@ extends Control
 
 const MAX_LEVEL: float = 30
 
+var main: PackedScene
+
 @onready var gpu_particles_2d: GPUParticles2D = %GPUParticles2D
 @onready var liquid: Liquid = %Liquid
 @onready var time_h_box_container: HBoxContainer = %TimeHBoxContainer
 @onready var level_h_box_container: HBoxContainer = %LevelHBoxContainer
 @onready var kills_h_box_container: HBoxContainer = %KillsHBoxContainer
+@onready var restart_button: Button = %RestartButton
 
 func _ready() -> void:
+	main = load("uid://da36exb4ldn5o")
+
+	restart_button.pressed.connect(_on_restart_button_pressed)
+
 	time_h_box_container.get_node("Amount").text = str(int(GameState.round_time_msec/60000.0)).pad_zeros(2)+":"+str(int(GameState.round_time_msec/1000.0)).pad_zeros(2)
 	level_h_box_container.get_node("Amount").text = str(GameState.round_level_reached)
 	kills_h_box_container.get_node("Amount").text = str(GameState.round_kills)
@@ -21,7 +28,7 @@ func _ready() -> void:
 		.set_trans(Tween.TRANS_CUBIC)\
 		.set_ease(Tween.EASE_IN)
 
-	await get_tree().create_timer(1.8).timeout
+	await get_tree().create_timer(2).timeout
 	liquid.splash(liquid.global_position + Vector2(liquid.liquid_size.x * randf(), 0), randi_range(350, 400) * liquid.splash_multiplier)
 	gpu_particles_2d.queue_free()
 
@@ -35,3 +42,8 @@ func _ready() -> void:
 	liquid.splash(liquid.global_position + Vector2(liquid.liquid_size.x * randf(), 0), randi_range(200, 400) * liquid.splash_multiplier)
 	await get_tree().create_timer(0.5).timeout
 	liquid.splash(liquid.global_position + Vector2(liquid.liquid_size.x * randf(), 0), randi_range(200, 400) * liquid.splash_multiplier)
+
+func _on_restart_button_pressed() -> void:
+	await ScreenTransition.to_black()
+	get_tree().change_scene_to_packed(main)
+	ScreenTransition.to_transparent()
